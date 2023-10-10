de02384bdb99c3470b1292583f5a83e73e66e2b3
Saw <ehkalanephaung96@gmail.com>
2023-10-10 17:05:22 +0700

    6.2

--- src/main/java/se331/lab/rest/repository/OrganizerRepository.java
+++ src/main/java/se331/lab/rest/repository/OrganizerRepository.java
@@ -1,7 +1,7 @@
 package se331.lab.rest.repository;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se331.lab.rest.entity.Organizer;
-
+import se331.lab.rest.entity.Event;
 public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
 }
--- src/main/java/se331/lab/rest/util/LabMapper.java
+++ src/main/java/se331/lab/rest/util/LabMapper.java
@@ -1,16 +1,25 @@
 package se331.lab.rest.util;
 
+import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.entity.EventDTO;
-
-import java.util.List;
+import se331.lab.rest.entity.Organizer;
+import se331.lab.rest.entity.OrganizerDTO;
 
 @Mapper
 public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
+
     EventDTO getEventDto(Event event);
+
     List<EventDTO> getEventDto(List<Event> events);
+
+    OrganizerDTO getOrganizerDTO(Organizer organizer);
+
+    List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
+
 }

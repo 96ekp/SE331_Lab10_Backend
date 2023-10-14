1717034555f6c727766eb44148ff609fae50f86f
Saw <ehkalanephaung96@gmail.com>
2023-10-14 21:27:30 +0700

    7

--- src/main/java/se331/lab/rest/entity/Participant.java
+++ src/main/java/se331/lab/rest/entity/Participant.java
@@ -1,9 +1,16 @@
 package se331.lab.rest.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder

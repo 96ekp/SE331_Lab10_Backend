3ff0ddcf2c5e79c142566716c6ef74d3fef75b73
Saw <ehkalanephaung96@gmail.com>
2023-10-15 22:56:02 +0700

    1.3

--- src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
@@ -1,15 +1,16 @@
 package se331.lab.rest.dao;
 
-import lombok.RequiredArgsConstructor;
+import static org.springframework.beans.support.PagedListHolder.*;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.repository.EventRepository;
-
-import static org.springframework.beans.support.PagedListHolder.DEFAULT_PAGE_SIZE;
 
 @Repository
 @RequiredArgsConstructor
@@ -28,34 +29,27 @@ public class EventDaoDbImpl implements EventDao {
             return eventRepository.findAll(PageRequest.of(page - 1, pageSize));
         } else {
             // Handle the case where "page" is null, e.g., return the first page by default
-            return eventRepository.findAll(PageRequest.of(0, pageSize != null? pageSize : DEFAULT_PAGE_SIZE));
+            return eventRepository.findAll(PageRequest.of(0, pageSize != null ? pageSize : DEFAULT_PAGE_SIZE));
         }
     }
-//    @Override
-//    public Page<Event> getEvents(Integer pageSize, Integer page) {
-//        return eventRepository.findAll(PageRequest.of(page-1,pageSize));
-//    }
-
+    // @Override
+    // public Page<Event> getEvents(Integer pageSize, Integer page) {
+    // return eventRepository.findAll(PageRequest.of(page-1,pageSize));
+    // }
 
     @Override
     public Event getEvent(Long id) {
         return eventRepository.findById(id).orElse(null);
     }
 
-
-
-
     @Override
-   public Event save(Event event) {
-                return eventRepository.save(event);
-            }
-
+    public Event save(Event event) {
+        return eventRepository.save(event);
+    }
 
     @Override
     public Page<Event> getEvents(String title, Pageable page) {
-     return eventRepository.findByTitle(title,page);
+        return eventRepository.findByTitle(title, page);
     }
 
-
 }
-

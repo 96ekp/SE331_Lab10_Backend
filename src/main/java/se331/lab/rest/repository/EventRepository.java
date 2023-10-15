7d41f2fbc4a5c69ca7a9573d30dd55988413d696
Saw <ehkalanephaung96@gmail.com>
2023-10-15 23:14:06 +0700

    1.6

--- src/main/java/se331/lab/rest/repository/EventRepository.java
+++ src/main/java/se331/lab/rest/repository/EventRepository.java
@@ -1,14 +1,20 @@
 package se331.lab.rest.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import se331.lab.rest.entity.Event;
 
-import java.util.List;
+import se331.lab.rest.entity.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAll();
+
     Page<Event> findByTitle(String title, Pageable pageRequest);
+
+    Page<Event> findByTitleContaining(String title, String description, Pageable pageRequest);
+    // Page<Event> findByTitleContainingOrDescriptionContaining(String title, String
+    // description, Pageable pageRequest);
 
 }
